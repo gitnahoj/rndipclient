3a8d8e7b149c37c7ab158a887f52f80b43194c91
John Doe <john@doe.org>
2022-05-04 20:47:55 +0300

    changed logic of workers to self receiving tasks

--- code/CheckProxy.py
+++ code/CheckProxy.py
@@ -6,6 +6,12 @@ import json
 
 #pip install urllib3==1.25.11
 
+checkDataUrl = sys.argv[1]
+proxies=[]
+jsondata={
+  "token":sys.argv[2]
+}
+
 def ip2int(addr):
     return struct.unpack("!I", socket.inet_aton(addr))[0]
 
@@ -16,20 +22,21 @@ def int_to_bytes(x: int) -> bytes:
   return x.to_bytes((x.bit_length() + 7) // 8, 'big')
 
 def sendProxyStatus(status):
+  print("--sendProxyStatus")
   global jsondata
   url="%s/%s/%s/%s/%s" % (jsondata["resp_url"],jsondata["token"],jsondata["proxy"]["ip"],jsondata["proxy"]["port"],status)
   response = requests.get(url)
   print("===========Send proxy status %s  responce code = %s" % (url,response.status_code))
-  exit()
+  #exit()
 
 def checkProxyByType(proxy_type):
+  print("--checkProxyByType")
   global jsondata
 
   headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
   }
   url = jsondata["check_url"]
-  #_proxy=proxy_type+"://"+proxy
   if proxy_type!="":
     _proxy="%s://%s" % (proxy_type,jsondata["proxy"]['full'])
     _proxies={"http": _proxy, "https": _proxy}
@@ -50,59 +57,82 @@ def checkProxyByType(proxy_type):
     print("error %s"%s)
   return False
 
+def loadProxyList():
+  print("--loadProxyList")
+  global checkDataUrl
+  global jsondata
+  url = "%s/getjob/%s/10" % (checkDataUrl,jsondata["token"])
 
-#print(sys.argv[1])
+  resp = requests.get(url=url) #, params=params)
+  print ("---load proxie list check data")
+  if(resp.status_code==200):
+    data = resp.json()
+    print("=== loaded %s proxies and %s ports " % (len(data["ip"]),len(data["ports"])))
+    jsondata["resp_url"] = checkDataUrl + data["resp_url"]
+    jsondata["check_url"] = data["trysite"]
+    jsondata["timeout"] = data["timeout"]
+    for ip in data["ip"]:
+      for port in data["ports"]:
+        proxies.append({"ip":ip,"port":int(port)})
 
-jsondata = json.loads(sys.argv[1])
-port=jsondata["proxy"]["port"]
-ip=int2ip(jsondata["proxy"]["ip"])
-jsondata["proxy"]["full"]="%s:%s" % (ip,port)
 
-#ip = socket.gethostbyname("nodesdirect.com")
-#payload_socks4 = b"\x04\x01"+int_to_bytes(443) +  socket.inet_aton(ip) + b"\x00"
-payload_socks5 = struct.pack('BBB',0x05, 0x01, 0x00)
+#///
+i=0
+while (i<100):
+  i=i+1
+  if (len(proxies)==0):
+    loadProxyList()
+  else:
+    proxy = proxies.pop()
+    jsondata["proxy"] = proxy
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(jsondata["timeout"])
+    port=proxy["port"]
+    ip=int2ip(proxy["ip"])
+    jsondata["proxy"]["full"]="%s:%s" % (ip,port)
+    print("curr proxy = %s" %  jsondata["proxy"])
+
+    #ip = socket.gethostbyname("nodesdirect.com")
+    #payload_socks4 = b"\x04\x01"+int_to_bytes(443) +  socket.inet_aton(ip) + b"\x00"
+    payload_socks5 = struct.pack('BBB',0x05, 0x01, 0x00)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(jsondata["timeout"])
   
-try:
-  s.connect((ip, port))
-except Exception as e:
-  s = str(e)
-  print("!!!error %s %s" % (jsondata["proxy"]["full"],s))
-  sendProxyStatus("0")
+    try:
+      s.connect((ip, port))
+    except Exception as e:
+      print("!!!error %s %s" % (jsondata["proxy"]["full"],str(e)))
+      sendProxyStatus("0")
+      continue
 
-print("---------------Request #%s--------------------------"%jsondata["proxy"])
-try:
-  #sen = struct.pack('BBB',0x04, 0x01, 0x00)
-  #payload = b"\x04\x01\x00\x50" +  socket.inet_aton(ip) + b"\x00"
-  #payload = b'\x04\x01P\xb8\xaf\xd7\x8d\x00'
+    print("---------------Request #%s--------------------------"%jsondata["proxy"])
+    try:
+      s.sendall(payload_socks5)
+      print("Payload 5 sended. Getting info..")
+      _data = s.recv(100)
+      print("socket sock5 received")
+      print(_data)
+      if(len(_data)==2):
+        version, auth = struct.unpack('BB', _data)
+        print("V=%s"%version)
+        print("auth=%s"%auth)
+        if (version==5 and auth==0):
+          sendProxyStatus("5")
+      s.close()
+    except socket.error as e:
+      print("socket socks4 error %s " %str(e))
+    except Exception as e:
+      print("fail socket socks5 %s" %str(e))
 
-  s.sendall(payload_socks5)
-  print("Payload 5 sended. Getting info..")
-  _data = s.recv(100)
-  print("socket sock5 received")
-  print(_data)
-  if(len(_data)==2):
-    version, auth = struct.unpack('BB', _data)
-    print("V=%s"%version)
-    print("auth=%s"%auth)
-    if (version==5 and auth==0):
+    if(checkProxyByType("")==True):
+      sendProxyStatus("1")
+      continue
+    elif checkProxyByType("socks4")==True:
+      sendProxyStatus("4")
+      continue
+    elif checkProxyByType("socks5")==True:
       sendProxyStatus("5")
-  s.close()
-except socket.error as e:
-  print("socket socks4 error")
-  print(str(e))
-except Exception as e:
-  s = str(e)
-  print("fail socket socks4")
-  print(s)
-
-if(checkProxyByType("")==True):
-  sendProxyStatus("1")
-elif checkProxyByType("socks4")==True:
-  sendProxyStatus("4")
-elif checkProxyByType("socks5")==True:
-  sendProxyStatus("5")
-else:
-  sendProxyStatus("0")
+      continue
+    else:
+      sendProxyStatus("0")
+      continue
--- code/main.py
+++ code/main.py
@@ -9,43 +9,13 @@ import json
 import sys
 
 checkDataUrl = sys.argv[1]
-proxies=[]
-resp_data={
-  "token":sys.argv[2]
-}
-#checkDataUrl = "http://v562757.macloud.host"
-
-def loadProxyList():
-  global checkDataUrl
-  global resp_data
-  url = "%s/getjob/%s/100" % (checkDataUrl,resp_data["token"])
-  #params = dict(
-   # token='rndtoken',
-  #  count=300,
-   #
-
-  resp = requests.get(url=url) #, params=params)
-  print ("---load proxie list check data")
-  if(resp.status_code==200):
-    data = resp.json()
-    print("=== loaded %s proxies and %s ports " % (len(data["ip"]),len(data["ports"])))
-    resp_data["resp_url"] = checkDataUrl + data["resp_url"]
-    resp_data["check_url"] = data["trysite"]
-    resp_data["timeout"] = data["timeout"]
-    for ip in data["ip"]:
-      for port in data["ports"]:
-        proxies.append({"ip":ip,"port":int(port)})
+token = sys.argv[2]
 
 while (True):
   loud_cpu, loud_mem = psutil.cpu_percent(), psutil.virtual_memory().percent
-  if (len(proxies))==0:
-    loadProxyList()
-  elif (loud_cpu <50 and loud_mem<90):
-    print("len(proxies) = %s"%len(proxies))
-    #print("----run process")
-    resp_data["proxy"]=proxies.pop()
-    #subprocess.Popen(["python","CheckProxy.py",json.dumps(resp_data)], creationflags=subprocess.CREATE_NEW_CONSOLE)
-    subprocess.Popen(["python3","CheckProxy.py",json.dumps(resp_data)])
+  if (loud_cpu <60 and loud_mem<90):
+    #subprocess.Popen(["python","CheckProxy2.py",checkDataUrl,token], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    subprocess.Popen(["python","CheckProxy2.py",checkDataUrl,token])
   else:
     print("====System too low. Waiting... cpu = %s memory = %s" % (loud_cpu, loud_mem))
-  time.sleep(0.1)  
+  time.sleep(2)  
