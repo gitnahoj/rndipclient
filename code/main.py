8fba8f4e6ae7480f8d1ad39f39a82a73d4463442
John Doe <john@doe.org>
2022-05-06 00:50:43 +0300

    changed to async

--- code/main.py
+++ code/main.py
@@ -6,16 +6,16 @@ import psutil
 import time
 import subprocess
 import json
-import sys
+import sys, os
 
-checkDataUrl = sys.argv[1]
-token = sys.argv[2]
+#checkDataUrl = sys.argv[1]
+#token = sys.argv[2]
 
 while (True):
   loud_cpu, loud_mem = psutil.cpu_percent(), psutil.virtual_memory().percent
   if (loud_cpu <60 and loud_mem<90):
     #subprocess.Popen(["python","CheckProxy.py",checkDataUrl,token], creationflags=subprocess.CREATE_NEW_CONSOLE)
-    subprocess.Popen(["python","CheckProxy.py",checkDataUrl,token])
+    os.spawnl(os.P_DETACH, sys.executable, sys.executable, "CheckProxy.py", "http://v562757.macloud.host ttt")# os.P_DETACH P_NOWAIT
   else:
     print("====System too low. Waiting... cpu = %s memory = %s" % (loud_cpu, loud_mem))
-  time.sleep(0.4)  
+  time.sleep(0.1)  
