2276bd8fd9cb08894b8a29d75dc2502a2d7187de
John Doe <john@doe.org>
2022-05-08 13:19:06 +0300

    not imnportant

--- code/main.py
+++ code/main.py
@@ -15,7 +15,7 @@ while (True):
   loud_cpu, loud_mem = psutil.cpu_percent(), psutil.virtual_memory().percent
   if (loud_cpu <60 and loud_mem<90):
     #subprocess.Popen(["python","CheckProxy.py",checkDataUrl,token], creationflags=subprocess.CREATE_NEW_CONSOLE)
-    os.spawnl(os.P_NOWAIT, sys.executable, sys.executable, "CheckProxy.py", "http://v562757.macloud.host ttt")# os.P_DETACH P_NOWAIT
+    os.spawnl(os.P_NOWAIT, sys.executable, sys.executable, "CheckProxy.py", "http://v562757.macloud.host", "ttt")# os.P_DETACH P_NOWAIT
   else:
     print("====System too low. Waiting... cpu = %s memory = %s" % (loud_cpu, loud_mem))
   time.sleep(0.1)  
